c1c835950a2178bc3a51a228f30fe0af19557ba5
toransahu <toran.sahu@yahoo.com>
2017-09-04 18:24:06 +0530

    updated

--- algo/dp/equal.py
+++ algo/dp/equal.py
@@ -3,18 +3,27 @@ DP Problem
 URL: https://www.hackerrank.com/challenges/equal
 """
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
 def min_ops(interns):
-    min_choc = min(intern)
-    max_choc = max(intern)
-    no_ops = 0
     solved = -1
-    if min_choc == max_choc:
-        return [no_ops,solved]
-    if max_choc - min_choc >= 5:
-
-
-
-    return [no_ops,solved]
+    no_ops = 0
+    while(solved == -1):
+        interns = sorted(interns, reverse=True)
+        min_choc = min(interns)
+        max_choc = max(interns)
+        if min_choc == max_choc:
+            solved = 1
+                
+        if max_choc - min_choc >= 5:
+            interns[0] = interns[0] - 5
+            no_ops+=1
+        elif max_choc - min_choc >= 2:
+            interns[0] = interns[0] - 2
+            no_ops+=1
+        elif max_choc - min_choc >= 1:
+            interns[0] = interns[0] - 1
+            no_ops+=1
+    return no_ops
 
 
 t = int(input().strip())
@@ -22,4 +31,4 @@ for _ in range(t):
     no_intern = int(input().strip())
     interns = sorted(map (int,input().strip().split(' ')))
     solved = False
-    no_ops =
+    print(min_ops(interns))
