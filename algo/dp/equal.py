27c12b6a53bbd4893e48be14a246d0ac4c3ba211
TORAN <toran.sahu@yahoo.com>
2017-09-04 10:23:51 +0530

    re-structured

---
+++ algo/dp/equal.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def min_ops(interns):
+    min_choc = min(intern)
+    max_choc = max(intern)
+    no_ops = 0
+    solved = -1
+    if min_choc == max_choc:
+        return [no_ops,solved]
+    if max_choc - min_choc >= 5:
+        
+
+
+    return [no_ops,solved]
+
+
+t = int(input().strip())
+for _ in range(t):
+    no_intern = int(input().strip())
+    interns = sorted(map (int,input().strip().split(' ')))
+    solved = False
+    no_ops =
