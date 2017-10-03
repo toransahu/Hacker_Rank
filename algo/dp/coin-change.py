bd219ef0eebdcf7b4c667170d74267853aa52cf1
toransahu <toran.sahu@yahoo.com>
2017-10-03 18:18:26 +0530

    updated

--- algo/dp/coin-change.py
+++ algo/dp/coin-change.py
@@ -3,16 +3,16 @@ import sys
 URL: https://www.hackerrank.com/challenges/coin-change
 """
 
-m = 4
-c = [8, 3, 1, 2]
-n = 4
+amount = 4
+no_of_deno = 4
+all_coins = [8, 3, 1, 2]
 # create list with 5 list and (m+1) element in each
-memo = [[0 for i in range(m + 1)] for j in range(5)]
-cn = [i for i in c if i <= n]
+memo = [[0 for i in range(no_of_deno + 1)] for j in range(no_of deno + 1)]
+coins = [i for i in all_coins if i <= amount]
 way = 0
 
-for i in cn:
-    for j in cn:
+for i in coins:
+    for j in ccoins:
         if i != j and (memo[i][j] == 1 or memo[j][i] == 1):
             way += 1
         elif i != j and i + j == n:
@@ -20,8 +20,8 @@ for i in cn:
             memo[j][i] = 1
             way += 1
 
-for i in range(n // min(cn)):
-    for j in cn:
-        if j * i == n:
+for i in range(amount // min(coins)):
+    for j in coins:
+        if j * i == amount:
             way += 1
 return way
