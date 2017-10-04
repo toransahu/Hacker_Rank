846ab3eb499523c94d352af0f27080dc90cc9343
toransahu <toran.sahu@yahoo.com>
2017-10-04 10:11:34 +0530

    v2

--- algo/dp/coin-change.py
+++ algo/dp/coin-change.py
@@ -1,27 +1,40 @@
+#!/bin/python3
+
 import sys
 """
 URL: https://www.hackerrank.com/challenges/coin-change
 """
 
-amount = 4
-no_of_deno = 4
-all_coins = [8, 3, 1, 2]
-# create list with 5 list and (m+1) element in each
-memo = [[0 for i in range(no_of_deno + 1)] for j in range(no_of_deno + 1)]
-coins = [i for i in all_coins if i <= amount]
-way = 0
+import sys
 
-for i in coins:
-    for j in coins:
-        if i != j and (memo[i][j] == 1 or memo[j][i] == 1):
-            way += 1
-        elif i != j and i + j == amount:
-            memo[i][j] = 1
-            memo[j][i] = 1
-            way += 1
 
-for i in range(amount // min(coins)):
-    for j in coins:
-        if j * i == amount:
-            way += 1
-print(way)
+def getWays(amount, all_coins):
+    # Complete this function
+    coins = [i for i in all_coins if i <= amount]
+    no_of_deno = len(coins)
+    memo = [[0 for i in range(no_of_deno + 1)] for j in range(no_of_deno + 1)]
+
+    way = 0
+
+    for i in coins:
+        for j in coins:
+            if i != j and (memo[i][j] == 1 or memo[j][i] == 1):
+                way += 1
+            elif i != j and i + j == amount:
+                memo[i][j] = 1
+                memo[j][i] = 1
+                way += 1
+
+    for i in range(amount // min(coins)):
+        for j in coins:
+            if j * i == amount:
+                way += 1
+    return way
+
+
+amount, no_of_deno = input().strip().split(' ')
+amount, no_of_deno = [int(amount), int(no_of_deno)]
+all_coins = list(map(int, input().strip().split(' ')))
+# Print the number of ways of making change for 'amount' units using coins having the values given by 'all_coins'
+ways = getWays(4, '1 2 3')
+print(ways)
