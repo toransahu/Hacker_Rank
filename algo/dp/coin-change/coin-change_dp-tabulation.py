2dcd17b6bfae7c4d1c61ff795b984681531df619
TORAN <toran.sahu@yahoo.com>
2018-06-19 09:38:03 +0530

    [updated] still need to improve

--- algo/dp/coin-change/coin-change_dp-tabulation.py
+++ algo/dp/coin-change/coin-change_dp-tabulation.py
@@ -13,11 +13,19 @@ URL: https://www.hackerrank.com/challenges/coin-change
 def getWays(amount, coins):
     """Find number of ways."""
     coins = sorted(coins)
+    # create a table of lenght amount + 1
+    # fill 0th with 1 i.e. way of making 0 rupay = 1
+    # fill rest with 0
+
     ways = [1] + [0] * amount
+
+    # find ways of each amount till given amount
     for coin in coins:
+        # start from current amount till amount given
         for i in range(coin, amount+1):
+            # ways of current amount = ways of current amount till now + ways of (current amount - current coin)
             ways[i] += ways[i - coin]
-    return count
+    return ways[amount]
 
 
 # amount, no_of_deno = input().strip().split(' ')
